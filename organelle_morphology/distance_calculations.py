2e0e9fba8ca82f0de90130ba6f96ddc6fe712499
Tim Schrader <tim.schrader@kit.edu>
2026-03-27 10:23:07 +0100

    fix: flatten bounding_boxed compute

--- organelle_morphology/distance_calculations.py
+++ organelle_morphology/distance_calculations.py
@@ -284,11 +284,7 @@ def generate_distance_matrix(
         meshes = persist(*meshes)
 
         with span("dist_matrix_bounding_boxes"):
-            # bounding_boxes = compute(bounding_boxes)[0]
-            # with many meshes (20k) ~30% faster then computing directly:
-            bounding_boxes = project.client.gather(
-                project.client.map(lambda m: m.compute().bounding_box.bounds, meshes)
-            )
+            bounding_boxes = [m.bounding_box.bounds for m in compute(*meshes)]
         print(f"bounding_boxes {len(bounding_boxes)}")
 
         project.logger.info("Calculating distance matrix")
